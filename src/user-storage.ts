151101f0e24988f30cb0ffa6e520b1b827cfa089
Denis Klimenko <jaschik26@yandex>
2022-06-19 22:16:09 +0400

    fix: storage

--- src/user-storage.ts
+++ src/user-storage.ts
@@ -10,36 +10,36 @@ interface User {
 
 export let users: Set<User> = new Set();
 
-users.add({
-  id: crypto.randomUUID(),
-  username: 'Vasa',
-  age: 11,
-  hobbies: [
-    'chess', 'tennis', 'swimming'
-  ]
-})
+// users.add({
+//   id: crypto.randomUUID(),
+//   username: 'Vasa',
+//   age: 11,
+//   hobbies: [
+//     'chess', 'tennis', 'swimming'
+//   ]
+// })
 
-users.add({
-  id: crypto.randomUUID(),
-  username: 'Kola',
-  age: 22,
-  hobbies: [
-    'chess'
-  ]
-})
+// users.add({
+//   id: crypto.randomUUID(),
+//   username: 'Kola',
+//   age: 22,
+//   hobbies: [
+//     'chess'
+//   ]
+// })
 
-users.add({
-  id: crypto.randomUUID(),
-  username: 'Vana',
-  age: 33,
-  hobbies: []
-})
+// users.add({
+//   id: crypto.randomUUID(),
+//   username: 'Vana',
+//   age: 33,
+//   hobbies: []
+// })
 
-users.add({
-  id: crypto.randomUUID(),
-  username: 'Peta',
-  age: 44,
-  hobbies: [
-    'chess', 'swimming'
-  ]
-})
+// users.add({
+//   id: crypto.randomUUID(),
+//   username: 'Peta',
+//   age: 44,
+//   hobbies: [
+//     'chess', 'swimming'
+//   ]
+// })
